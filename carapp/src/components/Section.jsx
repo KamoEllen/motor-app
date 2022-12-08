e5ad43ec75c206b6237e74e5104385a2669395f3
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-08 12:49:40 +0000

    Pending changes exported from your codespace

--- carapp/src/components/Section.jsx
+++ carapp/src/components/Section.jsx
@@ -1,19 +1,99 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Section = () => {
+const Section = ({title , description , leftBtnText , righBtnText ,  backgroundImg}) => {
+  
   return (
-     <wrap>
+     <Container bgImage={backgroundImg}>
         Section
-    </wrap>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+        <Buttons>
+        <ButtonGroup>
+          <LeftButton>
+            {leftBtnText}
+          </LeftButton>
+          <RightButton>
+            {righBtnText}
+          </RightButton>
+        </ButtonGroup>
+        
+          <DownArrow src="images/down-arrow.svg"/>
+          </Buttons>
+    </Container>
   )
 }
 
 export default Section
 
-const wrap = styled.div`
+const Container = styled.div`
 width: 100vh;
 height: 100vh;
-background-color: red;
-background-image: url('/images/model-s.jpg);
+background-size: cover;
+background-repeat: not-repeat;
+background-image: url('/images/model-s.jpg');
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+background-image: ${props => `url("/images/${props.bgImage}")`}
+
 `;
+
+const ItemText = styled.div`
+padding-top: 15vh;
+text-align: center;
+
+`;
+
+
+const LeftButton = styled.div`
+background-color: rgba(23, 26, 32, 0.8);
+height: 40px;
+width: 256px;
+color: silver;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 100px;
+opacity: 0.85;
+text-transform: uppercase;
+font-size: 12px;
+cusor: pointer;
+margin: 8px;
+
+
+`;
+
+const RightButton = styled(LeftButton)`
+background: white;
+opacity: 0.65;
+color: black;
+
+`;
+
+const ButtonGroup = styled.div`
+display: flex;
+margin-bottom: 30px;
+@media(max-width: 768px){
+  flex-direction: column;
+
+}
+
+`;
+
+const DownArrow = styled.img`
+height: 40px;
+overflow-x: hidden;
+animation: animateDown infinite 1.5s;
+
+
+`;
+
+const Buttons = styled.div`
+
+`;
+
+
