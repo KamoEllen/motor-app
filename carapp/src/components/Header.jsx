b9059614de7e0a4bdd477118473a4704f978c5aa
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-08 16:44:49 +0000

    Pending changes exported from your codespace

--- carapp/src/components/Header.jsx
+++ carapp/src/components/Header.jsx
@@ -1,11 +1,80 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <div>
+    <Container>
+
       header section
-    </div>
+      <a>
+        <img src="/images/logo.svg" alt=""/>
+      </a>
+      <Menu>
+       <a href="#">Model K</a>
+       <a href="#">Model A</a>
+       <a href="#">Model M</a>
+       <a href="#">Model O</a>
+       <a href="#">Model K</a>
+      </Menu>
+      <RightMenu>
+      <a href="#">Shop</a>
+      <a href="#">Tesla Account</a>
+      <CustomMenu/>
+      </RightMenu>
+    </Container>
   )
 }
-
+ 
 export default Header
+
+const Container = styled.div`
+min-height: 60px;
+position: fixed;
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+padding: 0 20px;
+top: 0;
+left: 0;
+right: 0;
+
+`;
+
+const Menu = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex: 1;
+
+a{
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 0 10px;
+  flex-wrap: nowrap;
+
+}
+
+@media(max-width: 768px){
+  display: none;
+
+}
+`;
+
+ const RightMenu = styled.div`
+ display: flex;
+ align-items: center;
+
+ a{
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 0 10px;
+
+}
+ `;
+
+ const CustomMenu = styled(CustomMenu)`
+ cusor:  pointer;
+
+ `;
+
--- carapp/src/components/Section3.jsx
+++ carapp/src/components/Section3.jsx
@@ -2,47 +2,47 @@ import React from 'react'
 import styled from 'styled-components'
 import Fade from 'react-reveal/Fade'
 
-const Section = ({title , description , leftBtnText , righBtnText ,  backgroundImg}) => {
+const Section3 = ({title , description , leftBtnText , righBtnText ,  backgroundImg}) => {
   
   return (
-     <Container bgImage={backgroundImg}>
-        <Fade bottom>
-        <ItemText>
-          <h1>{title}</h1>
-          <p>{description}</p>
-        </ItemText>
-        </Fade>
-        <Buttons>
-          <Fade bottom>
-        <ButtonGroup>
-          <LeftButton>
-            {leftBtnText}
-          </LeftButton>
-          <RightButton>
-            {righBtnText}
-          </RightButton>
-        </ButtonGroup>
-        
-          <DownArrow src="images/down-arrow.svg"/>
-          </Fade>
-          </Buttons>
-    </Container>
+    <Container bgImage={backgroundImg}>
+    <Fade bottom>
+    <ItemText>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </ItemText>
+    </Fade>
+    <Buttons>
+      <Fade bottom>
+    <ButtonGroup>
+      <LeftButton>
+        {leftBtnText}
+      </LeftButton>
+      <RightButton>
+        {righBtnText}
+      </RightButton>
+    </ButtonGroup>
+    
+      <DownArrow src="images/down-arrow.svg"/>
+      </Fade>
+      </Buttons>
+</Container>
   )
 }
 
-export default Section
+export default Section3
 
 const Container = styled.div`
 width: cover;
 height: 100vh;
 background-size: cover;
 background-repeat: not-repeat;
-background-image: url('https://i.pinimg.com/564x/78/42/67/78426767e2cc56cf006e978cec113659.jpg');
+background-image: url('https://i.pinimg.com/564x/0a/ed/d6/0aedd6fa2d6a6472b4971df3871d2c53.jpg');
 display: flex;
 flex-direction: column;
 justify-content: space-between;
 align-items: center;
-background-image: url('https://i.pinimg.com/564x/78/42/67/78426767e2cc56cf006e978cec113659.jpg');
+background-image: url('https://i.pinimg.com/564x/0a/ed/d6/0aedd6fa2d6a6472b4971df3871d2c53.jpg')
 
 `;
 
@@ -99,5 +99,4 @@ animation: animateDown infinite 1.5s;
 const Buttons = styled.div`
 
 `;
-
 
