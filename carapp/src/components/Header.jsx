88422d38b5497d14a44b06f0960e7af250c03812
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-08 14:11:57 +0000

    Pending changes exported from your codespace

--- carapp/src/components/Header.jsx
+++ carapp/src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 const Header = () => {
   return (
-    <div>
+    <Container>
+
       header section
-    </div>
+      <a>
+        <img src="/images/logo.svg" alt=""/>
+      </a>
+      <Menu>
+       <p><a href="#">Model K</a></p>
+       <p><a href="#">Model A</a></p>
+       <p><a href="#">Model M</a></p>
+       <p><a href="#">Model O</a></p>
+       <p><a href="#">Model K</a></p>
+      </Menu>
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div``;
+
+const Menu = styled.div``;
